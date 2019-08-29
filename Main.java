76d96a119fb498e3a71595a873cd91284afe1b86
bmatthews1 <bmatthews1@unm.edu>
2019-08-29 13:42:19 -0600

    Added some comments

--- Main.java
+++ Main.java
@@ -2,17 +2,33 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
+
   TreeMap<Integer, ArrayList<String>> list = new TreeMap<Integer, ArrayList<String>>(); 
 
+  /**
+    Main method that initializes the class
+  */
   public static void main(String[] args){
     Main m = new Main();
     m.init();
   }
 
+  /**
+    Method to initialize all the needed variables and 
+    call all the needed methods in a sequential manner. 
+  */
   public void init(){
     readFile();
   }
 
+  /**
+    Reads and parses csv file into data structure in the form of
+    a map where the key is the index/id number and the value is
+    the list of data.
+
+    Note: Change the file name from "test.csv" to your file name
+    and make sure it is saved in the same folder as this java file.
+  */
   public void readFile(){
     try{
       BufferedReader csvReader = new BufferedReader(new FileReader("test.csv"));
@@ -24,9 +40,7 @@ public class Main{
               ArrayList<String> data = new ArrayList<String>(Arrays.asList(index[i].split(";")));
               list.put(Integer.parseInt(index[0]), data);
             }
-          }
-          // do something with the data
-          
+          }          
       }
       System.out.println(list.get(4).size());
       csvReader.close();
